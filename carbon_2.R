d3f17f65f8fc12e95b931b10e394199e073a2024
AnnaClaire Marley <armarley@ESM.UCSB.EDU>
2019-12-02 13:38:54 -0800

    added csv files of pre and post and worked on discount

--- carbon_2.R
+++ carbon_2.R
@@ -10,73 +10,12 @@ library(RODBC)
 ## ensure that plot ids are read in as entire number (vs being auto-converted to scientific notation)
 options(scipen = 999)
 
-##################
-## read in data ##
-##################
+################################
+## read in data cleaned data ##
+#############################
 
-conn <- odbcConnectAccess2007("scenario_results_allrx.mdb")
-cost <- sqlFetch(conn, "harvest_costs", as.is = T)
-odbcCloseAll()
-
-conn <- odbcConnectAccess2007("optimizer_results_all_cycles_MaxTotal_Stand_CarbonNRgtP0_2019-11-03_10-20-26 (1).accdb")
-acres <- sqlFetch(conn, "all_cycles_best_rx_summary", as.is = T)
-odbcCloseAll()
-
-
-
-conn <- odbcConnectAccess2007("PREPOST_FVS_CARBON.ACCDB")
-pre_carb <- sqlFetch(conn, "PRE_FVS_CARBON", as.is = T)
-post_carb <- sqlFetch(conn, "POST_FVS_CARBON", as.is = T)
-odbcCloseAll()
-
-
-
-conn <- odbcConnectAccess2007("PREPOST_FVS_HRV_CARBON.ACCDB")
-pre_carb_hrv <- sqlFetch(conn, "PRE_FVS_HRV_CARBON", as.is = T)
-post_carb_hrv <- sqlFetch(conn, "POST_FVS_HRV_CARBON", as.is = T)
-odbcCloseAll()
-
-
-####################
-## select columns ##
-####################
-
-## optimized packages + harvest cost per acre
-cost_sel <- cost %>% select(biosum_cond_id, rxpackage, complete_cpa)
-# by only selective harvest_cpa we are explicity ignoring transportation
-
-## stand carbon
-pre_carb_sel <- pre_carb %>% 
-  select(biosum_cond_id, rxpackage, rxcycle, Total_Stand_Carbon)
-
-post_carb_sel <- post_carb %>% 
-  select(biosum_cond_id, rxpackage, rxcycle, Total_Stand_Carbon)
-
-
-## harvest carbon
-pre_hrv_sel <- pre_carb_hrv %>% 
-  select(biosum_cond_id, rxpackage, rxcycle, Merch_Carbon_Removed)
-
-post_hrv_sel <- post_carb_hrv %>% 
-  select(biosum_cond_id, rxpackage, rxcycle, Merch_Carbon_Removed)
-
-
-## join carbon tables
-pre_carbon_tot <- left_join(pre_carb_sel, pre_hrv_sel)  %>% 
-  distinct()
-post_carbon_tot <- left_join(post_carb_sel, post_hrv_sel)  %>% 
-  distinct()
-
-## sum stand and harvest carbon to get total
-pre_carbon_tot$tot_all <- pre_carbon_tot$Total_Stand_Carbon 
-post_carbon_tot$tot_all <- post_carbon_tot$Total_Stand_Carbon
-
-
-### join to econ
-pre_full <- left_join(cost_sel, pre_carbon_tot)  %>% 
-  distinct()
-post_full <- left_join(cost_sel, post_carbon_tot)  %>% 
-  distinct()
+pre_full <- read_csv("pre_harv_full.csv")
+post_full <- read_csv("post_harv_full.csv")
 
 ####################
 #carbon discounting
@@ -86,9 +25,9 @@ post_full <- left_join(cost_sel, post_carbon_tot)  %>%
 
 # filter down to just one plot
 plot_pre <- pre_full %>% 
-  filter(biosum_cond_id=="1200506050501500846430001") %>% 
+  filter(biosum_cond_id==1200506050501500909686420) %>% 
   filter(rxpackage=="001") %>% 
-  tail(4) %>% 
+  filter(complete_cpa!=0) %>% # need to change this
   select(-tot_all) %>% 
   mutate(time = rxcycle)
 
@@ -105,9 +44,9 @@ for (i in length(plot_pre$time)){
 
 #### for post
 plot_post <- post_full %>% 
-  filter(biosum_cond_id=="1200506050501500846430001") %>% 
+  filter(biosum_cond_id==1200506050501500909686420) %>% 
   filter(rxpackage=="001") %>% 
-  tail(4) %>% 
+  filter(complete_cpa!=0) %>% ## need to change this
   select(-tot_all) %>% 
   mutate(time = rxcycle) 
 
@@ -121,31 +60,36 @@ for (i in length(plot_post$time)){
   
 }
 
-# now combine pre and post by time and make sure time is an integer
-plot_pre_post <- tibble(time = 0:31)
-plot_pre_post$time <- as.integer(plot_pre_post$time)
-plot_pre$time <- as.integer(plot_pre$time)
-plot_post$time <- as.integer(plot_post$time)
-plot_pre_post <- left_join(plot_pre_post, plot_post) 
-plot_pre_post <- left_join(plot_pre_post, plot_pre)
+#stack pre and post on top of each other
+pre_post <- rbind(plot_pre, plot_post)
 
-# expand out to get each year carbon increments
-plot_post_expand <- plot_post %>% 
+#add columns that show to each year carbon increments
+# ** we want to look at from cycle 1--> 10, 11 --> 20, 21--> 30 so ignore the #
+# changes from 0-->1 etc bc that's merchantable **** #
+
+pre_post <- pre_post %>% 
+  # make sure in the right order
+  arrange(time) %>% 
   # find the difference between the the stand carbon in cycle 1 and 2, 2 and 3 etc
   mutate(diff = Total_Stand_Carbon - lag(Total_Stand_Carbon, default = first(Total_Stand_Carbon))) %>%
   # what happens each year
-  mutate(each_year = diff/10) 
+  mutate(each_year = diff/10)
 
+# now create a new dataframe with all of the times and make sure time is an integer
+plot_all <- tibble(time = 0:31)
 
-# need to make a new dataframe that subtracts 
-plot_discount <- tibble(time = 0:31)
-plot_discount <- left_join(plot_discount, plot_post_expand) 
-plot_discount <- left_join(plot_discount, plot1)
+# now merge together the dataframes
+plot_all <- left_join(plot_all, pre_post, by = "time") %>% 
+  mutate(stand_carbon_exp = Total_Stand_Carbon)
 
-for (i in length(plot_discount$stand_c_expand)){
-  plot_discount$stand_c_expand = ifelse(plot_discount$stand_c_expand[1,11], 
-                                        plot_discount$Total_Stand_Carbon - plot_discount$Total_Stand_Carbon)
+# now need to fill in the empty total stand carbon columns 
+for (i in 2:nrow(plot_all)){
+  plot_all$stand_carbon_exp = ifelse(plot_all$time <=9, 
+                                       plot_all$stand_carbon_exp[i-1] - 1.3,
+                                       plot_all$stand_carbon_exp)
 }
+
+
 
 
 
