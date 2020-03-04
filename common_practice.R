07e4630cba39dac0beb2dd47e8bfc105d4b47e96
AnnaClaire Marley <armarley@ESM.UCSB.EDU>
2020-03-03 16:30:25 -0800

    trying to add spatial data

--- common_practice.R
+++ common_practice.R
@@ -1,6 +1,25 @@
 library(openxlsx)
 library(tidyverse)
+library(sf)
 
-areas <- read.xlsx("assessment_area_data_file.xlsx", fillMergedCells = TRUE)
+####################################################################
+# first perform a spatial join of plot locations into supersections#
+####################################################################
+
+# read in the shapefles
+supersections_sub <- read_sf("spatial_data", layer = "supersections_subset_b")
+plot_loc_geom <- read_sf("spatial_data", layer = "plot_loc")
+
+# make supersections have the same crs as plot locations
+st_crs(supersections_sub) = 4326
+
+plot(supersections_sub)
+plot(plot_loc_geom)
+
+
+# join the supersecon to the points
+plot_loc_ss <- st_join(plot_loc_geom, supersections_sub)
+
+area<- read_csv("assessment_area_data.csv")
 
 forest_type <- read_csv("plot_loc.csv")
